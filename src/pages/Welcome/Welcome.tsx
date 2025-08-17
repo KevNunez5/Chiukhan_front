2c46cfda86d0e19ef7d999c0adcdb46a13e0de2f
Nivek <kevin500nunez@gmail.com>
2025-08-16 18:15:55 -0600

    Pagina welcome lista mas que nada

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -4,9 +4,11 @@ export default function Welcome() {
     return (
         <div className="welcome-container">
             <div className="welcome-content">
+                <div className="spacer"></div>
                 <h1>
                     Tu nueva plataforma de aprendizaje con Inteligencia Artificial Offline
                 </h1>
+                <div className="spacer"></div>
                 <button className="start-button">¡Comenzar!</button>
             </div>
         </div>
